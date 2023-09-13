225267f1e284677bb56b5c56c7c4542752ef1b79
shanitahgithub <142230234+shanitahgithub@users.noreply.github.com>
2023-09-13 18:40:35 +0300

    exercise.py

---
+++ assignment 1/ex.py
@@ -0,0 +1,46 @@
+q=2,4,6 # even numbers less than 8
+print("The sum of even numbers less than 8 is" + "" + str(sum(q)))
+
+# Average of five numbers
+num1=5
+num2=7
+num3=8
+num4=4
+num5=6
+total=num1+num2+num3+num4+num5
+average=total/5
+print("Their average is" + str(average))
+
+# Operations
+-5+8*6
+a=-5
+b=8
+c=6
+d=b*c
+print(a+d)
+
+(55+9)%9
+a=55
+b=9
+n=(a+b)
+print(n%b)
+
+20+-3*5/8
+a=((-3*5)/8)+20
+print(a)
+
+5+15/3*2-8%3
+b=3
+a=(2*(15/3)+5)-8
+print(a%b)
+
+# Arraylist of animals
+import numpy as np
+animals_array=np.array(["cows", "goats", "sheep"])
+print(animals_array)
+
+
+
+
+
+
